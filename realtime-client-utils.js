919a1afd2a153e3b80fb8a2352ca167c1da2a8a4
Seth Howard <smiles.seth@gmail.com>
2015-02-18 09:26:04 -0700

    Updated license

--- realtime-client-utils.js
+++ realtime-client-utils.js
@@ -3,6 +3,18 @@
  * https://developers.google.com/drive/realtime/
  * Copyright 2015 Seth Howard, Google Inc. All Rights Reserved.
  * Realtime Utils may be freely distributed under the Apache 2.0 license.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 /**
